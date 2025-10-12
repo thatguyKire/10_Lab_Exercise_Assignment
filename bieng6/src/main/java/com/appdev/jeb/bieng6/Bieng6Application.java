39700082da6e1c13d2d4d40ee6343e037fe98d24
thatguyErik <eriksamuelbien@gmail.com>
2025-10-12 12:55:14 +0800

    Initial commit for Act10 - Spring Boot project

---
+++ bieng6/src/main/java/com/appdev/jeb/bieng6/Bieng6Application.java
@@ -0,0 +1,13 @@
+package com.appdev.jeb.bieng6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bieng6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bieng6Application.class, args);
+	}
+
+}
---
+++ bieng6/src/test/java/com/appdev/jeb/bieng6/Bieng6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdev.jeb.bieng6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bieng6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
